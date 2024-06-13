0a679abb4639f78fb6fe51703de3731f39a7224f
Simon Aronsson <simme@arcticbit.se>
2024-06-13 16:50:16 +0200

    Update make-classic.sh

--- make-classic.sh
+++ make-classic.sh
@@ -3,5 +3,7 @@
 sed -i 's/base: core22/base: core20/g' snap/snapcraft.yaml
 sed -i 's/confinement: strict/confinement: classic/g' snap/snapcraft.yaml
 sed -i 's/CRAFT_PART_INSTALL/SNAPCRAFT_PART_INSTALL/g' snap/snapcraft.yaml
+sed -i 's/architectures/platforms/g' snap/snapcraft.yaml
+sed -i -E 's/- (build-on: (\w+))/\2:\n    \1/g' snap/snapcraft.yaml    
 sed -i '/libbpfcc/d' snap/snapcraft.yaml
 sed -i '/bpfcc-tools/d' snap/snapcraft.yaml
