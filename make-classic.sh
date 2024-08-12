bf24f92c3146dcaa9a28b11cf022434f56188e2c
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2024-08-12 16:16:38 +0000

    add quotes

--- make-classic.sh
+++ make-classic.sh
@@ -3,7 +3,7 @@
 sed -i 's/base: core22/base: core20/g' snap/snapcraft.yaml
 sed -i 's/confinement: strict/confinement: classic/g' snap/snapcraft.yaml
 sed -i 's/CRAFT_PART_INSTALL/SNAPCRAFT_PART_INSTALL/g' snap/snapcraft.yaml
-sed -i 's/    command: agent-wrapper/    command: ${SNAP}\/agent -config.expand-env -config.file \/etc\/grafana-agent.yaml/g' snap/snapcraft.yaml
+sed -i 's/    command: agent-wrapper/    command: "${SNAP}\/agent -config.expand-env -config.file \/etc\/grafana-agent.yaml"/g' snap/snapcraft.yaml
 sed -i '/libbpfcc/d' snap/snapcraft.yaml
 sed -i '/bpfcc-tools/d' snap/snapcraft.yaml
 yq -i 'del(.apps.grafana-agent.plugs) | del(.plugs)' snap/snapcraft.yaml
