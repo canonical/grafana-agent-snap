2cb61c76bb3ed3ce0b8653c0706b96d1fac2e622
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2024-08-28 12:49:55 +0300

    Fix wrapper (#72)

--- make-classic.sh
+++ make-classic.sh
@@ -6,3 +6,8 @@ sed -i 's/CRAFT_PART_INSTALL/SNAPCRAFT_PART_INSTALL/g' snap/snapcraft.yaml
 sed -i '/libbpfcc/d' snap/snapcraft.yaml
 sed -i '/bpfcc-tools/d' snap/snapcraft.yaml
 yq -i 'del(.apps.grafana-agent.plugs) | del(.plugs)' snap/snapcraft.yaml
+cat << "EOF" > snap/local/agent-wrapper
+#!/bin/sh
+
+exec "${SNAP}/agent" -config.expand-env -config.file "/etc/grafana-agent.yaml"
+EOF
