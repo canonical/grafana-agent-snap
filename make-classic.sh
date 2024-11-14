a6528beadf34359f33d908e04d2726df6c343ce8
sed-i <82407168+sed-i@users.noreply.github.com>
2024-11-14 17:09:27 -0500

    Fixes

--- make-classic.sh
+++ make-classic.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-sed -i 's/base: core22/base: core20/g' snap/snapcraft.yaml
 sed -i 's/confinement: strict/confinement: classic/g' snap/snapcraft.yaml
-sed -i 's/CRAFT_PART_INSTALL/SNAPCRAFT_PART_INSTALL/g' snap/snapcraft.yaml
-sed -i '/libbpfcc/d' snap/snapcraft.yaml
-sed -i '/bpfcc-tools/d' snap/snapcraft.yaml
 yq -i 'del(.apps.grafana-agent.plugs) | del(.plugs)' snap/snapcraft.yaml
 sed -i 's/agent-wrapper.strict/agent-wrapper.classic/g' snap/snapcraft.yaml
