1a063484091d270481a96fceb8ab7a5658cd780e
Leon <82407168+sed-i@users.noreply.github.com>
2024-11-15 10:05:48 -0500

    Fix classic snap (#80)
    * Refactor to reduce errors
    * Revert to building classic on core20

--- make-classic.sh
+++ make-classic.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
+# The classic snap needs to be built on core20 because of libc6 dependency:
+# A core22 snap needs glibc 2.32+, but in a 20.04 charm we have glibc 2.31.
 sed -i 's/base: core22/base: core20/g' snap/snapcraft.yaml
 sed -i 's/confinement: strict/confinement: classic/g' snap/snapcraft.yaml
 sed -i 's/CRAFT_PART_INSTALL/SNAPCRAFT_PART_INSTALL/g' snap/snapcraft.yaml
 sed -i '/libbpfcc/d' snap/snapcraft.yaml
 sed -i '/bpfcc-tools/d' snap/snapcraft.yaml
 yq -i 'del(.apps.grafana-agent.plugs) | del(.plugs)' snap/snapcraft.yaml
-cat << "EOF" > snap/local/agent-wrapper
-#!/bin/sh
-
-exec "${SNAP}/agent" -config.expand-env -config.file "/etc/grafana-agent.yaml"
-EOF
+sed -i 's/agent-wrapper.strict/agent-wrapper.classic/g' snap/snapcraft.yaml
