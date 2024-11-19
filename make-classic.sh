4165b25ff7a693e74dbf94775dc8151533bb4427
sed-i <82407168+sed-i@users.noreply.github.com>
2024-11-18 23:39:20 -0500

    Use go-channel for core20

--- make-classic.sh
+++ make-classic.sh
@@ -8,4 +8,9 @@ sed -i 's/CRAFT_PART_INSTALL/SNAPCRAFT_PART_INSTALL/g' snap/snapcraft.yaml
 sed -i '/libbpfcc/d' snap/snapcraft.yaml
 sed -i '/bpfcc-tools/d' snap/snapcraft.yaml
 yq -i 'del(.apps.grafana-agent.plugs) | del(.plugs)' snap/snapcraft.yaml
+
+# For the classic snap, we need to use the golang deb rather than snap,
+# because in remote-build the snap version pin has no effect.
+yq -i 'del(.parts.grafana-agent."build-snaps") | .parts.grafana-agent."go-channel" = "1.22/stable"' snap/snapcraft.yaml
+
 sed -i 's/agent-wrapper.strict/agent-wrapper.classic/g' snap/snapcraft.yaml
