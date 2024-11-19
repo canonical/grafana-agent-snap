027777e5297b85721a4ff884680678eb3ee5e5ba
sed-i <82407168+sed-i@users.noreply.github.com>
2024-11-19 00:08:42 -0500

    Consolidate agent wrapper script

--- make-classic.sh
+++ make-classic.sh
@@ -2,7 +2,6 @@
 
 sed -i 's/confinement: strict/confinement: classic/g' snap/snapcraft.yaml
 yq -i 'del(.apps.grafana-agent.plugs) | del(.plugs)' snap/snapcraft.yaml
-sed -i 's/agent-wrapper.strict/agent-wrapper.classic/g' snap/snapcraft.yaml
 
 # With the classic snap we need to patchelf for the core22 snap to run on a 20.04 charm,
 # because of the libc6 dependency, otherwise the app will fail to start, complaining
