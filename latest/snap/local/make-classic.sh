499a6976b1daef5c8fe439f997fdda69dc8ad9f9
Luca Bello <luca.bello@canonical.com>
2026-05-12 16:19:12 +0200

    feat: add latest/ folder and update to new ADR structure
    - Add latest/ folder (copy of 0.44/) per ADR 2026-05-12
    - Update justfile with ADR-compliant update logic
    - Update snaps.just and spread.yaml to match blueprint

---
+++ latest/snap/local/make-classic.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sed -i 's/confinement: strict/confinement: classic/g' snap/snapcraft.yaml
+yq -i 'del(.apps.grafana-agent.plugs) | del(.plugs)' snap/snapcraft.yaml
+
+# With the classic snap we need to patchelf for the core22 snap to run on a 20.04 charm,
+# because of the libc6 dependency, otherwise the app will fail to start, complaining
+# about needing glibc 2.32+.
+yq -i '.parts.grafana-agent."build-attributes" += ["enable-patchelf"]' snap/snapcraft.yaml
